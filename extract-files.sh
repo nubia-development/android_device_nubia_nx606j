a6c2e870023327cd9bde7d1e80cb866842cfd9bc
SGCMarkus <markusornik@gmail.com>
2019-09-13 08:27:12 +0200

    nx606j: pin modified audio.primary.sdm845.so
    added libprocessgroup dependency which is now required for set_sched_policy, it moved from libcutils to libprocessgroup
    
    Change-Id: Ibfc37e46436185e6cf1fe95ddb6ba3cb7366fbbc

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib/hw/audio.primary.sdm845.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    vendor/lib64/hw/audio.primary.sdm845.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
