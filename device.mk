909ef06f2e1932e5fbe07fdd7630e84038fc7919
Cyborg2017 <wangyang0987654@gmail.com>
2023-05-08 22:37:03 +0800

    nx606j: Define the capabilities of the LEDs
    Change-Id: Ie35f5e7138f4d00dbacb1092224c3833b4561559

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
