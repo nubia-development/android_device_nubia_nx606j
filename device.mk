9899f0d32feb60176e2fbafc9473e654c0d3f9c9
Cyborg2017 <wangyang0987654@gmail.com>
2023-05-08 20:34:52 +0800

    nx606j: Build vibrator HIDL service from source
    Change-Id: Ie4b3bea31ea515f97f5f8211a55fe2b96e930948
    
    Change-Id: I0c78002f83867d1e2a7e37c5b34e1ec8bb4064e4

--- device.mk
+++ device.mk
@@ -43,3 +43,7 @@ PRODUCT_PACKAGES += \
     init.nubia.device.rc \
     fstab.qcom
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
